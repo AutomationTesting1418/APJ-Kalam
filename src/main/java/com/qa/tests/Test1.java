e44694d1aeb035a74f01072c246a246f3e9b92d2
AutomationTesting1418 <AutomationTesting1418@gmail.com>
2023-04-08 12:13:26 +0530

    Hi Changes have been made in Test1

--- src/main/java/com/qa/tests/Test1.java
+++ src/main/java/com/qa/tests/Test1.java
@@ -3,7 +3,7 @@ package com.qa.tests;
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to GITHUB");
+		System.out.println("Welcome to GITHUB account");
 	}
 
 }
